c93fb62df6e334f0bc8858080237fccee9040ee2
Theerttha <theerttha.s@gmail.com>
2025-08-24 22:47:51 +0530

    redirects

--- frontend/src/pages/Socket.jsx
+++
@@ -1,9 +0,0 @@
-// src/socket.js
-import { io } from "socket.io-client";
-
-const backend_url = import.meta.env.VITE_URL || "http://localhost:3000";
-
-export const socket = io(backend_url, {
-  transports: ["websocket"],
-  autoConnect: true,
-});
